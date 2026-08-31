48171c8abc6d575e23bdde3fdf2c9ebab00f9114
qhai-dev <qhai.dev@gmail.com>
2026-08-31 22:13:03 +0800

    todo

---
+++ frontend/apps/ios/App/App.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by 罗创 on 2026/8/31.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/apps/ios/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios
+//
+//  Created by 罗创 on 2026/8/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world! bazel and apple ios swift dev host 111")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
